433cec91c35b00c5913f4c89d6d075d66d9aabe7
dilsoncampelo10 <dilsono316@gmail.com>
2023-10-19 06:48:18 -0300

    :tada: init commit

---
+++ src/main/java/com/dilson/micromail/MicromailApplication.java
@@ -0,0 +1,13 @@
+package com.dilson.micromail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicromailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicromailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilson/micromail/MicromailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilson.micromail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicromailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
