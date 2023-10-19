a100ad4c4b1de7b0fa5d01992235b01dd875e6a1
dilsoncampelo10 <dilsono316@gmail.com>
2023-10-19 16:01:59 -0300

    email controller

---
+++ src/main/java/com/dilson/micromail/controllers/EmailController.java
@@ -0,0 +1,34 @@
+package com.dilson.micromail.controllers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dilson.micromail.dtos.EmailDto;
+import com.dilson.micromail.models.Email;
+import com.dilson.micromail.services.EmailService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("emails")
+public class EmailController {
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/send")
+    public ResponseEntity<Email> send(@RequestBody @Valid EmailDto emailDto) {
+        var email = new Email();
+
+        BeanUtils.copyProperties(emailDto, email);
+
+        emailService.sendMail(email);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(email);
+    }
+}
